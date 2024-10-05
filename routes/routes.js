74c59f711471eba5889147d457bea4fef6e48c81
Alex Ulloa <US21003@ues.edu.sv>
2024-10-04 22:19:52 -0600

    POST -> GET /api/init

--- routes/routes.js
+++ routes/routes.js
@@ -8,6 +8,6 @@ router.get("/users", apiKeyMiddleware, getAllUsers);
 router.get("/users/:id", apiKeyMiddleware, getUserById);
 router.get("/verifyUser", apiKeyMiddleware, verifyUser);
 router.put("/users/:id", apiKeyMiddleware, changeStatus);
-router.post("/init", init);
+router.get("/init", init);
 
 export default router;
